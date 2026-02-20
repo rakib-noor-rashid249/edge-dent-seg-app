99d8b7b3ab84a537e1116d82e492e4d2827827ed
pranta-barua007 <prantabarua247@gmail.com>
2026-02-21 00:27:53 +0600

    fix: rolled back to previous implementation

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export", // Enables static exports
-  basePath: process.env.NODE_ENV === "production" ? '/edge-dent-seg-app' : undefined, // Add your repo name as base path if needed
-  images: {
-    unoptimized: true, // Optional: for image optimization compatibility in static export
-  },
-  experimental: {
-    browserDebugInfoInTerminal: true,
-  },
+  // experimental: {
+  //   browserDebugInfoInTerminal: true,
+  // },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
